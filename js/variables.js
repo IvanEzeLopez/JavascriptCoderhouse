9b75edcb3d757670c24c4768efd142c3ea394bcd
ivanezlopez <ivan.ezlopez@gmail.com>
2022-07-24 09:26:43 -0300

    arreglos de formato

--- js/variables.js
+++ js/variables.js
@@ -2,14 +2,15 @@
 let nombreUsuario = prompt("Cuál es tu nombre?");
 console.log("Hola" + " " + nombreUsuario);
 alert("Hola" + " " + nombreUsuario);
+
 // Donde vive
 let paisUsuario = prompt("¿En qué país vivís" + "," + " " + nombreUsuario + "?");
 let provinciaUsuario = prompt("¿En qué provincia vivís" + "," + " " + nombreUsuario + "?");
 console.log("Ah, vivís en" + " " + provinciaUsuario + "," + " " + paisUsuario);
 alert("Ah, vivís en" + " " + provinciaUsuario + "," + " " + paisUsuario);
+
 // Edad
 let edad = Number(prompt("¿Qué edad tenés?"));
-
 console.log(edad + 10);  
 console.log(edad + 10 + " " + "años" + " " +  "vas a tener dentro de 10 años" + "," + " " + nombreUsuario );  
 alert(edad + 10 + " " + "años" + " " +  "vas a tener dentro de 10 años" + "," + " " + nombreUsuario );  

b683c945e3b653e568202891c20ca07473a27ac9
ivanezlopez <ivan.ezlopez@gmail.com>
2022-07-24 09:17:10 -0300

    Desafio 1

--- js/variables.js
+++ js/variables.js
@@ -1,59 +1,15 @@
-// soy un comentario
-console.log("Hola mundo");
-console.log(2 + 3);
+// Nombre y saludo
+let nombreUsuario = prompt("Cuál es tu nombre?");
+console.log("Hola" + " " + nombreUsuario);
+alert("Hola" + " " + nombreUsuario);
+// Donde vive
+let paisUsuario = prompt("¿En qué país vivís" + "," + " " + nombreUsuario + "?");
+let provinciaUsuario = prompt("¿En qué provincia vivís" + "," + " " + nombreUsuario + "?");
+console.log("Ah, vivís en" + " " + provinciaUsuario + "," + " " + paisUsuario);
+alert("Ah, vivís en" + " " + provinciaUsuario + "," + " " + paisUsuario);
+// Edad
+let edad = Number(prompt("¿Qué edad tenés?"));
 
-/* y yo soy un comenteario
-multilinea */
-
-// Variables
-//Definición: Es un espacio en memoria en donde se va a guardar información
-
-// Declarar variables (EM5)
-var nombre = "Martin"; //string
-
-
-// Declarar variables (EM6)
-//Con "let" podemnos cambiar su contenido (NO redleclarar)
-
-let apellido = "Gonzalez";
-
-/* let apellido (Declaración)
-apellido = "Perez" (Asignación)
- */
-const PI = 3.14;
-
-
-// const PI_TEXTO = "3.14";
-// const PI = 3.14;
-
-// console.log(PI_TEXTO);
-// console.log(PI_NRO);
-
-/* Operaciones matemáticas */
-/* Operaciones con datos numericos */
-
-const base = 12;
-const altura = 3;
-
-const areaRectangulo = base * altura;
-const perimetro = 2 * base + 2 * altura;
-const resta = base - altura;
-const division = base / altura;
-const potencia = base ** 2;
-const raiz = Math.sqrt(144); // square root
-
-/* console.log(areaRectangulo);
-console.log(perimetro);
-console.log(resta);
-console.log(division);
-console.log(potencia);
-console.log(raiz); */
-
-/* console.log(PI)
-
-console.log("Hola" + " " + nombre + " " + apellido) */
-
-let nombreUsuario = prompt("Decinos tu nombre");
-
-console.log("hola "+nombreUsuario)
-alert("hola "+nombreUsuario)
+console.log(edad + 10);  
+console.log(edad + 10 + " " + "años" + " " +  "vas a tener dentro de 10 años" + "," + " " + nombreUsuario );  
+alert(edad + 10 + " " + "años" + " " +  "vas a tener dentro de 10 años" + "," + " " + nombreUsuario );  
